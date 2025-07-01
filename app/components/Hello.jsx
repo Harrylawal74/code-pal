b21a2d3c5acc1d253f7830995ad671aa9e5de7a8
Harry Lawal <harrylawal74@gmail.com>
2025-07-01 16:45:57 +0100

    update - buttons component

--- app/components/Hello.jsx
+++
@@ -1,4 +0,0 @@
-function Hello(){
-    return <div className="text-2xl text-center text-blue-500">hello world</div>;
-    }
-export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import Hello from "./components/Hello";
+import Hello, {Bye} from "./components/Buttons";
 import LevelTab from "./components/LevelTab";
 
 
 export default function Home() {
   return (
-   <div><Hello />
+   <div>
+    <Hello />
+    <Bye/>
    <span className="inline-grid grid-cols-3 gap-4">
     <LevelTab />
     <LevelTab />
