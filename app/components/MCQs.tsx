37258461bb40916896a87a9d16d5f96c50dbe6e8
Harry Lawal <harrylawal74@gmail.com>
2025-11-21 19:28:32 +0000

    UI Update

--- app/components/MCQs.tsx
+++ app/components/MCQs.tsx
@@ -34,17 +34,17 @@ export default function MCQs({question}: {question: Question}) {
 
 
     return(
-        <form onSubmit={handleSubmit} className="mx-5 mt-20 bg-gradient-to-b from-[#121723] to-[#131623] px-5 py-7 rounded-4xl gap-3 max-w-150 w-full max-h-[700px] flex flex-col items-center border-2 border-[#2d2c47]" >
+        <form onSubmit={handleSubmit} className="mx-5 mt-20 bg-gradient-to-b from-[#121723] to-[#131623] px-10 py-15 rounded-4xl gap-3 max-w-200 w-full max-h-[700px] flex flex-col items-center border-2 border-[#2d2c47]" >
 
             
-            <h2 className="flex items-center justify-center text-white text-2xl font-bold bg-gray-500 rounded-4xl px-10 h-17 mb-15 gap-6 w-full ">{question.question}</h2>
+            <h2 className="flex items-center justify-center text-white text-2xl font-bold rounded-4xl px-10 h-17 mb-15 gap-6 w-full ">{question.question}</h2>
                 
             {/**Loops through each option and its index
              * Renders a <label> and <input> for each option
              */}
             {question.options.map((option, index) => (
             <label key={index}
-            className={" cursor-pointer bg-gradient-to-r from-[#1b202d] to-[#171c29] rounded-2xl px-10 h-17 flex items-center gap-6 w-full mb-5 shadow-md shadow-black hover:bg-gray-400 hover:border-1 hover:border-[#2d2c47]"}>
+            className={"cursor-pointer bg-gradient-to-r from-[#1b202d] to-[#171c29] hover:from-[#212632] hover:to-[#1d222e] rounded-2xl px-10 h-30 flex items-center gap-6 w-full mb-5 shadow-sm shadow-black hover:bg-gray-400 border-1 border-[#1a202b] hover:border-[#3b395a] hover:translate-x-[5px]  transition-all duration-300 "}>
                 
                 {/** Sets the state 'selectedAnswer to be the option that the user has selected
                  * and checks the option that the user has selected (option appears selected on form)
--- app/components/MarkButton.tsx
+++ app/components/MarkButton.tsx
@@ -6,6 +6,6 @@ export default function MarkButton() {
 
     /**const correctAnswer = ;*/
     return (
-        <button type="submit" className="bg-[var(--color-button-purple)] px-5 py-2 rounded-3xl flex justify-center items-center">Mark :)</button>
+        <button type="submit" className="w-full h-[70px] shadow-xl shadow-purple-900 hover:shadow-2xl bg-gradient-to-r from-[#a283f9] to-[#8f63f7] px-5 py-2 rounded-2xl flex justify-center items-center hover:translate-y-[-3px] transition-all duration-400">Next Question</button>
     )
 }
