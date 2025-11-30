a606d13421fd6c0999c23254abdb9c0ad01a9591
Harry Lawal <harrylawal74@gmail.com>
2025-11-30 08:01:10 +0000

    Foundations page cleanup
    Foundations page renders exercise boxes based in the number of exercises in the level data

--- app/(with-sidebar)/levels/foundations/page.tsx
+++ app/(with-sidebar)/levels/foundations/page.tsx
@@ -2,24 +2,22 @@ import Link from "next/link";
 import ExerciseBox from "../../../components/ExerciseBox";
 import level1 from "../../../data/leveldata/level1";
 import React from "react";
+import { Question } from "../../../types/Questions";
 
-
-
-
-type Props = { 
-  title: string; 
-}
+type Props = {
+  title: string;
+};
 
 /** This is the ExerciseSectionTab Template
  * * It is used to display a section of exercieses within a level.
  */
 
-function ExerciseSectionTabTemplate({title}: Props) {
-    return(
-        <div className="h-20 rounded-xl bg-[#2a6aee] flex justify-center items-center text-white text-sm md:text-xl sm:text-lg font-bold">
-            {title}
-        </div>
-    )
+function ExerciseSectionTabTemplate({ title }: Props) {
+  return (
+    <div className="h-20 rounded-xl bg-[#2a6aee] flex justify-center items-center text-white text-sm md:text-xl sm:text-lg font-bold">
+      {title}
+    </div>
+  );
 }
 
 /** To-do
@@ -27,53 +25,40 @@ function ExerciseSectionTabTemplate({title}: Props) {
  * *Copy the method used in the LevelTabTemplate to pass props to the template. above^^^
  */
 
-
-
 export default function FoundationsPage() {
   return (
     /** The main tag is the whole area minus the sidebar (when visible) items are stacked on top of each other
-     * Items are centered 
+     * Items are centered
      */
-    <main className="flex-col justify-center overflow-auto w-full h-[calc(100vh-96px)] bg-gray-700 rounded-3xl m-5 p-20 no-scrollbar"> 
-
-        {/**This container holds the two columns in the center of the page, items inside are centered 
-         * This container will grow wide enough to fit its contents without wrapping (min-w-max)
-         * This container will shrink down but not below the minimum width of its contents (min-w-max)
-        */}
-        <div className="flex justify-center overflow-auto gap-15 min-w-max px-4 ">
-        
-            {/* main exercises column. e.g. syntax basics followed by exercise squares */}
-            {/*This column (left) shrinks down to a fixed with and is always in page view */}
-            <div className="flex justify-center min-w-[212px] ">
-                <section className="mb-8">
-                <ExerciseSectionTabTemplate title={level1.sections[0].title}/>
-                    <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
-                        <Link href="/exercises/1-1-1-1">
-                        <ExerciseBox />
-                        </Link>
-                        <Link href="/exercises/1-1-1-5">
-                        <ExerciseBox />
-                        </Link>
-                        <Link href="/exercises/1-1-1-7">
-                        <ExerciseBox />
-                        </Link>
-                        <ExerciseBox />
-                        <ExerciseBox />
-                        <ExerciseBox />
-                        <ExerciseBox />
-                        <ExerciseBox />
-                        <ExerciseBox />
-                        <ExerciseBox />
-                    </div>
-                </section>
+    <main className="flex-col justify-center overflow-auto w-full h-[calc(100vh-96px)] bg-gray-700 rounded-3xl m-5 p-20 no-scrollbar">
+      {/**This container holds the two columns in the center of the page, items inside are centered
+       * This container will grow wide enough to fit its contents without wrapping (min-w-max)
+       * This container will shrink down but not below the minimum width of its contents (min-w-max)
+       */}
+      <div className="flex justify-center overflow-auto gap-15 min-w-max px-4 ">
+        {/* main exercises column. e.g. syntax basics followed by exercise squares */}
+        {/*This column (left) shrinks down to a fixed with and is always in page view */}
+        <div className="flex justify-center min-w-[212px] ">
+          <section className="mb-8">
+            <ExerciseSectionTabTemplate title={level1.sections[0].title} />
+            <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+              {level1.sections[0].exercises[0].questions.map(
+                (question, index) => (
+                  <Link key={index} href={`/exercises/${question.id}`}>
+                    <ExerciseBox />
+                  </Link>
+                )
+              )}
             </div>
+          </section>
+        </div>
 
-            {/* This column does not shrink or grow when the page is resized. it has a fixed width and overflows off the the
+        {/* This column does not shrink or grow when the page is resized. it has a fixed width and overflows off the the
             right when the page is too small*/}
-            <div className="flex justify-center flex-shrink-0 w-[364px] bg-gray-500 rounded-3xl h-200 pt-10">
-                Some stuff i havent thought about yet
-            </div>
-            </div>
+        <div className="flex justify-center flex-shrink-0 w-[364px] bg-gray-500 rounded-3xl h-200 pt-10">
+          Some stuff i havent thought about yet
+        </div>
+      </div>
     </main>
   );
 }
