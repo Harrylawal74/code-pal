f9a8b4e31be070f6f7e3f98593342b4b3238ecda
Harry Lawal <harrylawal74@gmail.com>
2025-12-07 12:16:51 +0000

    Update level1.ts
    Update level1 data for testing

--- app/data/leveldata/level1.ts
+++ app/data/leveldata/level1.ts
@@ -40,33 +40,58 @@ const level1: Level = {
               correctAnswer: "A set of instructions for a computer",
             },
             {
-              /* Level 1 Section 1 Exercise 1 Question 2*/
-              id: "1-1-1-2",
-              nextQuestionId: "1-1-1-3",
-              type: "MCQ",
-              question: "What is a variable?",
-              options: [
-                "A value that can change",
-                "A fixed value",
-                "A type of function",
-                "A programming language",
-                "A programming language",
-              ],
-              correctAnswer: "A value that can change",
+              /* Level 1 Section 1 Exercise 1 Question 2 */
+              id: "1-1-1-7",
+              nextQuestionId: "1-1-1-8",
+              type: "FIBCode",
+              question: "Complete the sentence:",
+              options: [], // not used for FIB
+              correctAnswer: "", // not used for FIB
+              fibData: {
+                sentenceParts: [
+                  { id: 1, text: "#line 1|", isBlank: false },
+                  { id: 2, text: "for i in range", isBlank: false },
+                  { id: 3, text: "", isBlank: true },
+                  { id: 4, text: "\n#line 2|", isBlank: false },
+                  { id: 5, text: '    print("Hello world!")', isBlank: false },
+                  { id: 6, text: "\n#line 3|", isBlank: false },
+                  { id: 7, text: "", isBlank: true },
+                ],
+                options: [
+                  { id: 1, text: "(0, 4)" },
+                  { id: 2, text: "#end of code" },
+                ],
+                correctBlanks: [
+                  { id: 3, correctOptionId: 1 },
+                  { id: 7, correctOptionId: 2 },
+                ],
+              },
             },
             {
-              /* Level 1 Section 1 Exercise 1 Question 3*/
-              id: "1-1-1-3",
-              nextQuestionId: "1-1-1-4",
-              type: "MCQ",
-              question: "What is a function?",
-              options: [
-                "A reusable block of code",
-                "A type of variable",
-                "A programming language",
-                "A type of software",
-              ],
-              correctAnswer: "A reusable block of code",
+              /* Level 1 Section 1 Exercise 1 Question 3 */
+              id: "1-1-1-5",
+              nextQuestionId: "1-1-1-6",
+              type: "FIB",
+              question: "Complete the sentence:",
+              options: [], // not used for FIB
+              correctAnswer: "", // not used for FIB
+              fibData: {
+                sentenceParts: [
+                  { id: 1, text: "Python is a", isBlank: false },
+                  { id: 2, text: "", isBlank: true },
+                  { id: 3, text: "that is used to make", isBlank: false },
+                  { id: 4, text: "", isBlank: true },
+                  { id: 5, text: ".", isBlank: false },
+                ],
+                options: [
+                  { id: 1, text: "programming language" },
+                  { id: 2, text: "algorithms" },
+                ],
+                correctBlanks: [
+                  { id: 2, correctOptionId: 1 },
+                  { id: 4, correctOptionId: 2 },
+                ],
+              },
             },
             {
               /* Level 1 Section 1 Exercise 1 Question 4*/
